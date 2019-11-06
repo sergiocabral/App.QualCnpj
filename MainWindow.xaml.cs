c871e1c3f184009a70d3058802ebcde5531c1d12
Sergio Cabral <sergio.cabral@sapiensia.com>
2019-11-06 17:32:45 -0300

    Comentários e formatação.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,36 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace QualCnpj
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private WebEngine _webEngine = new WebEngine();
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            textBoxOutput.Text = _webEngine.GetCnpjForAll(textBoxSource.Text);
+            TextBoxOutput.Text = WebEngine.GetCnpjForAll(TextBoxSource.Text);
         }
     }
 }
--- WebEngine.cs
+++ WebEngine.cs
@@ -10,27 +10,33 @@ namespace QualCnpj
     /// <summary>
     /// Agrupa as funcionalidade Web.
     /// </summary>
-    internal class WebEngine
+    internal static class WebEngine
     {
-        public string GetCnpjForAll(string text)
+        public static string GetCnpjForAll(string text)
         {
             if (string.IsNullOrWhiteSpace(text)) return string.Empty;
 
-            var list = text.Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
+            var companies = text.Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
+            
             var result = new StringBuilder();
-            foreach (var item in list)
+            foreach (var company in companies)
             {
-                result.AppendLine(GetCnpj(item));
+                result.AppendLine(GetCnpj(company));
             }
             return result.ToString();
         }
 
-        private string GetCnpj(string item)
+        /// <summary>
+        /// Obtem os dados de um CNPJ.
+        /// </summary>
+        /// <param name="names">Nome da empresa.</param>
+        /// <returns>Lista de CNPJ relacionados.</returns>
+        private static string GetCnpj(string names)
         {
-            var html = GetHtml($"https://www.google.com/search?q=cnpj.rocks:+{item}");
+            var html = GetHtml($"https://www.google.com/search?q=cnpj.rocks:+{names}");
             
             var result = new StringBuilder();
-            result.AppendLine(item);
+            result.AppendLine(names);
 
             foreach (var cnpj in ExtractAllCnpj(html).GroupBy(a => a).OrderByDescending(a => a.Count()).Select(a => a.FirstOrDefault()))
             {
@@ -41,14 +47,24 @@ namespace QualCnpj
             return result.ToString();
         }
 
-        private string FormatCnpj(string cnpj) =>
+        /// <summary>
+        /// Formata a exibição de um CNPJ.
+        /// </summary>
+        /// <param name="cnpj">CNPJ.</param>
+        /// <returns>CNPJ formatado.</returns>
+        private static string FormatCnpj(string cnpj) =>
             !Regex.IsMatch(cnpj, "[0-9]{14}") ? 
                 cnpj : 
                 $"{cnpj.Substring(0, 2)}.{cnpj.Substring(2, 3)}.{cnpj.Substring(5, 3)}/{cnpj.Substring(8, 4)}-{cnpj.Substring(12, 2)}";
 
-        private IEnumerable<string> ExtractAllCnpj(string html)
+        /// <summary>
+        /// Extrai todos os CNPJ contidos em um texto. 
+        /// </summary>
+        /// <param name="text">Texto.</param>
+        /// <returns>Lista de CNPJ</returns>
+        private static IEnumerable<string> ExtractAllCnpj(string text)
         {
-            foreach (Match match in Regex.Matches(html, @"[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}-?[0-9]{2}", RegexOptions.Singleline))
+            foreach (Match match in Regex.Matches(text, @"[0-9]{2}\.?[0-9]{3}\.?[0-9]{3}\/?[0-9]{4}-?[0-9]{2}", RegexOptions.Singleline))
                 yield return Regex.Replace(match.Value, "[^0-9]", string.Empty);
         }
 
@@ -57,7 +73,7 @@ namespace QualCnpj
         /// </summary>
         /// <param name="url">Página URL.</param>
         /// <returns>Código HTML.</returns>
-        public static string GetHtml(string url)
+        private static string GetHtml(string url)
         {
             var webClient = new WebClient();
             var response = webClient.DownloadString(new Uri(url));
